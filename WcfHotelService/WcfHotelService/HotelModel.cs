c9e7cb6ba6cedbd5fffcb396f7415b006271cbf8
Saurabh Manchanda <smanchanda@tavisca.com>
2018-09-18 14:26:37 +0530

    Hotel WCF Services

---
+++ WcfHotelService/WcfHotelService/HotelModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfHotelService
+{
+    [DataContract]
+    public class HotelModel
+    {
+        [DataMember]
+        public int HotelID { get; set; }
+        [DataMember]
+        public string HotelName { get; set; }
+        [DataMember]
+        public string HotelAddress { get; set; }
+        [DataMember]
+        public double HotelRating { get; set; }
+    }
+}
